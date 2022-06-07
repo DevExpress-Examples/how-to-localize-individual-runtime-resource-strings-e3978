ec8e6fced153825e56155addf97f65662f17a27c
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-07 18:38:45 +0400

    Update example

--- CS/WpfApplication24/MainWindow.xaml.cs
+++ CS/WpfApplication24/MainWindow.xaml.cs
@@ -1,14 +1,26 @@
-﻿using System.Windows;
+﻿using DevExpress.Xpf.Grid;
+using System.Windows;
 
 namespace WpfApplication24 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            gridControl1.ItemsSource = new ProductList();
-            gridControl1.View.ShowColumnChooser();
+            grid.ItemsSource = ProductList.GetProducts();
+        }
+        public void LocalizeGroupPanelText() {
+            var NewText = textBox.Text;
+            var localization = new GridRuntimeStringCollection();
+            localization.Add(new RuntimeStringIdInfo(GridControlRuntimeStringId.GridGroupPanelText, NewText));
+            view.RuntimeLocalizationStrings = localization;
+        }
+        private void button_Click(object sender, RoutedEventArgs e) {
+            LocalizeGroupPanelText();
+        }
+
+        private void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter) {
+                LocalizeGroupPanelText();
+            }
         }
     }
 }
--- CS/WpfApplication24/Products.cs
+++ CS/WpfApplication24/Products.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 public class Product {
     public string ProductName { get; set; }
@@ -9,17 +8,18 @@ public class Product {
     public int Quantity { get; set; }
 }
 
-public class ProductList : ObservableCollection<Product> {
-    public ProductList()
-        : base() {
-        Add(new Product() { ProductName = "Chang", Country = "UK", City = "Cowes", UnitPrice = 19, Quantity = 10 });
-        Add(new Product() { ProductName = "Gravad lax", Country = "Italy", City = "Reggio Emilia", UnitPrice = 12.5, Quantity = 16 });
-        Add(new Product() { ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro", UnitPrice = 19, Quantity = 12 });
-        Add(new Product() { ProductName = "Tarte au sucre", Country = "Germany", City = "QUICK-Stop", UnitPrice = 22, Quantity = 50 });
-        Add(new Product() { ProductName = "Steeleye Stout", Country = "USA", City = "Reggio Emilia", UnitPrice = 18, Quantity = 20 });
-        Add(new Product() { ProductName = "Pavlova", Country = "Austria", City = "Graz", UnitPrice = 21, Quantity = 52 });
-        Add(new Product() { ProductName = "Longlife Tofu", Country = "USA", City = "Boise", UnitPrice = 7.75, Quantity = 120 });
-        Add(new Product() { ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.", UnitPrice = 21, Quantity = 15 });
-        Add(new Product() { ProductName = "Alice Mutton", Country = "Canada", City = "Tsawwassen", UnitPrice = 44, Quantity = 16 });
+public class ProductList {
+    public static ObservableCollection<Product> GetProducts() {
+        ObservableCollection<Product> products = new ObservableCollection<Product>();
+        products.Add(new Product() { ProductName = "Chang", Country = "UK", City = "Cowes", UnitPrice = 19, Quantity = 10 });
+        products.Add(new Product() { ProductName = "Gravad lax", Country = "Italy", City = "Reggio Emilia", UnitPrice = 12.5, Quantity = 16 });
+        products.Add(new Product() { ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro", UnitPrice = 19, Quantity = 12 });
+        products.Add(new Product() { ProductName = "Tarte au sucre", Country = "Germany", City = "QUICK-Stop", UnitPrice = 22, Quantity = 50 });
+        products.Add(new Product() { ProductName = "Steeleye Stout", Country = "USA", City = "Reggio Emilia", UnitPrice = 18, Quantity = 20 });
+        products.Add(new Product() { ProductName = "Pavlova", Country = "Austria", City = "Graz", UnitPrice = 21, Quantity = 52 });
+        products.Add(new Product() { ProductName = "Longlife Tofu", Country = "USA", City = "Boise", UnitPrice = 7.75, Quantity = 120 });
+        products.Add(new Product() { ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.", UnitPrice = 21, Quantity = 15 });
+        products.Add(new Product() { ProductName = "Alice Mutton", Country = "Canada", City = "Tsawwassen", UnitPrice = 44, Quantity = 16 });
+        return products;
     }
 }
