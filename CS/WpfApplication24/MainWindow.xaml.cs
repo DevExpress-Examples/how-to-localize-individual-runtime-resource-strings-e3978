2b773258924a0b03fc52bd673af88babb0d5a018
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-08 12:05:15 +0400

    Update sample

--- CS/WpfApplication24/MainWindow.xaml.cs
+++ CS/WpfApplication24/MainWindow.xaml.cs
@@ -7,17 +7,17 @@ namespace WpfApplication24 {
             InitializeComponent();
             grid.ItemsSource = ProductList.GetProducts();
         }
-        public void LocalizeGroupPanelText() {
+        void LocalizeGroupPanelText() {
             var NewText = textBox.Text;
             var localization = new GridRuntimeStringCollection();
             localization.Add(new RuntimeStringIdInfo(GridControlRuntimeStringId.GridGroupPanelText, NewText));
             view.RuntimeLocalizationStrings = localization;
         }
-        private void button_Click(object sender, RoutedEventArgs e) {
+        void Button_Click(object sender, RoutedEventArgs e) {
             LocalizeGroupPanelText();
         }
 
-        private void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+        void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
             if (e.Key == System.Windows.Input.Key.Enter) {
                 LocalizeGroupPanelText();
             }
