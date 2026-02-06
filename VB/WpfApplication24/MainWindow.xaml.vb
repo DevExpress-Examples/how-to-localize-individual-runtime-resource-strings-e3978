4d9c205d362ae08d30ef757eb23e81e567e24575
DevExpressExampleBot <fakeEmail>
2026-02-06 14:18:09 +0400

    Source auto update [skip ci]

--- VB/WpfApplication24/MainWindow.xaml.vb
+++ VB/WpfApplication24/MainWindow.xaml.vb
@@ -1,16 +1,17 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace WpfApplication24
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			gridControl1.ItemsSource = New ProductList()
-			gridControl1.View.ShowColumnChooser()
-		End Sub
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.gridControl1.ItemsSource = New ProductList()
+            Me.gridControl1.View.ShowColumnChooser()
+        End Sub
+    End Class
 End Namespace
